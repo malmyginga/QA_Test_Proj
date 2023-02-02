df813e90f5331ae0381a46e5ee040e25ef9607ff
Gleb Malmygin <malmygingleb1@gmail.com>
2023-02-02 17:52:00 +0300

    Make webtests headless

--- src/test/java/webtests/BaseTest.java
+++ src/test/java/webtests/BaseTest.java
@@ -1,5 +1,13 @@
 package webtests;
 
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeSuite;
+
 public class BaseTest {
     protected String urlPath = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+
+    @BeforeSuite
+    public void setRunHeadless() {
+        Configuration.headless = true;
+    }
 }
